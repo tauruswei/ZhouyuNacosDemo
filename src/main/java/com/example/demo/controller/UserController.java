7221480a2d19d7cf315f4ce5dcaaacf557df37dc
fengxiaoxiao <13156050650@163.com>
2022-11-23 09:57:34 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+    @Value("${server.port}")
+    private String port;
+    @GetMapping ("/hello")
+    public String test(){
+        return "zhouyu"+port;
+    }
+}
